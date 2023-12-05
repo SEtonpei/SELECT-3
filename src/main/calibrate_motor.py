ddfdec0c2955502b9cdacaab8cff2db898d873e9
Ryo-Kuzuno <kuzuno1027@gmail.com>
2023-12-06 02:49:15 +0900

    modify pin_servo statements

--- src/main/calibrate_motor.py
+++ src/main/calibrate_motor.py
@@ -3,7 +3,7 @@ sys.path.append('../library/')
 from selemod import Actuator
 
 pin_esc = 18
-pin_servo_1 = 23
+pin_servo = 23
 # pin_servo_2 = 24
 freq_esc = 50 
 freq_servo = 50
@@ -13,7 +13,7 @@ brakeoff_duty = 4.85
 pin_ec2_top = 16 
 pin_ec2_bottom = 20
 
-actu = Actuator(pin_esc=pin_esc, pin_servo_1=pin_servo_1, 
+actu = Actuator(pin_esc=pin_esc, pin_servo=pin_servo, 
                 freq_esc=freq_esc, freq_servo=freq_servo, 
                 brakeon_duty=brakeon_duty, brakeoff_duty=brakeoff_duty)
 
