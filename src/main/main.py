da473640c1efcaa354424ad28d9c7eae7b4ca2c5
Ryo-Kuzuno <kuzuno1027@gmail.com>
2024-11-23 10:57:00 +0900

    Change throttle 3

--- src/main/main.py
+++ src/main/main.py
@@ -4,7 +4,7 @@ from mission import Resilience
 
 
 def main(): 
-    distance = 20 # in meter  
+    distance = 30 # in meter  
     reduce_rate = 0.2   # Ratio of stop height to distance
     spec = {"radius": 0.03, "height": 2, "gear_ratio_enc2roller": 1} # in meter
     sensor = {"bme" : False, "sht" : False, "counter" : True}
